c2726bc5434474c34fbf572c4f0a70f812eba412
GemmechuBekele <gemmechu.bekele.berga@gmail.com>
2025-05-17 15:09:32 +0300

    Modify performance file

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -26,39 +26,14 @@ SELECT
 
 FROM Booking b
 JOIN [User] u ON b.user_id = u.user_id
-JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
+JOIN [Property] p ON b.property_id = p.property_id
+LEFT JOIN [Payment] pay ON b.booking_id = pay.booking_id
 
--- Analyze the query’s performance using EXPLAIN
-EXPLAIN ANALYZE
-SELECT 
-    b.booking_id,
-    b.start_date,
-    b.end_date,
-    b.total_price,
-    b.status,
-    b.created_at AS booking_created_at,
+-- Add WHERE and AND to limit rows and simulate a real query
+WHERE b.status = 'Confirmed'
+  AND b.created_at >= NOW() - INTERVAL '30 days'
+ORDER BY b.created_at DESC;
 
-    u.user_id,
-    u.first_name,
-    u.last_name,
-    u.email,
-    u.role,
-
-    p.property_id,
-    p.name AS property_name,
-    p.location,
-    p.price_per_night,
-
-    pay.payment_id,
-    pay.amount,
-    pay.payment_method,
-    pay.payment_date
-
-FROM Booking b
-JOIN [User] u ON b.user_id = u.user_id
-JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
 
 -- Reducing unnecessary joins or using indexing
 CREATE INDEX idx_booking_user_id ON Booking(user_id);
