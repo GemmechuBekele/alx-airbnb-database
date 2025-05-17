93dda6fd25224d4a6bf41e5a7ac85ff2e8492a6d
GemmechuBekele <gemmechu.bekele.berga@gmail.com>
2025-05-17 09:23:21 +0300

    Modify joins_queries file

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -18,8 +18,26 @@ FROM booking b INNER JOIN "user" u ON b.user_id = u.user_id;
 
 -- 2. LEFT JOIN: Get all properties and their reviews (even if no reviews)
 
-SELECT *
-FROM property  LEFT JOIN review  ON property.property_id = review.property_id;
+SELECT 
+p.property_id,
+p.host_id,
+p.name AS property_name,
+p.description,
+p.location,
+p.price_per_night,
+p.created_at AS property_created_at,
+p.update_at AS property_updated_at,
+r.review_id,
+r.user_id AS reviewer_id,
+r.rating,
+r.comment,
+r.created_at AS review_created_at
+FROM property p 
+LEFT JOIN review r 
+ON p.property_id = r.property_id
+ORDER BY
+p.name ASC,
+r.created_at ASC;
 
 -- 3. FULL OUTER JOIN: All users and all bookings (even if unmatched)
 -- Query for PostgreSQL (which supports FULL OUTER JOIN natively)
